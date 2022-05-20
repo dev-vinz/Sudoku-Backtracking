49162c864fe9306c3cfc071a8ed65d7eceff034e
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-05-20 09:29:11 +0200

    Initial dev commit

---
+++ GridView/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridView
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private const int InnerWidth = 3;
+		private const int OuterWidth = InnerWidth * InnerWidth;
+
+		private const int Thin = 1;
+		private const int Thick = 3;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			InitializeGridModel();
+			InitializeSudokuTable();
+		}
+
+		public Grid GridModel => (Grid)DataContext;
+
+		private void InitializeGridModel()
+		{
+			DataContext = new Grid();
+		}
+
+		private void InitializeSudokuTable()
+		{
+			var grid = new UniformGrid
+			{
+				Rows = Grid.NB_CELLS,
+				Columns = Grid.NB_CELLS
+			};
+
+			for (var i = 0; i < Grid.NB_CELLS; i++)
+			{
+				for (var j = 0; j < Grid.NB_CELLS; j++)
+				{
+					var border = CreateBorder(i, j);
+					border.Child = CreateTextBox(i, j);
+					grid.Children.Add(border);
+				}
+			}
+
+			SudokuTable.Child = grid;
+		}
+
+		private static Border CreateBorder(int i, int j)
+		{
+			var left = j % InnerWidth == 0 ? Thick : Thin;
+			var top = i % InnerWidth == 0 ? Thick : Thin;
+			var right = j == OuterWidth - 1 ? Thick : 0;
+			var bottom = i == OuterWidth - 1 ? Thick : 0;
+
+			return new Border
+			{
+				BorderThickness = new Thickness(left, top, right, bottom),
+				BorderBrush = Brushes.Black
+			};
+		}
+
+		private TextBox CreateTextBox(int i, int j)
+        {
+			var textBox = new TextBox
+			{
+				VerticalAlignment = VerticalAlignment.Center,
+				HorizontalAlignment = HorizontalAlignment.Center
+			};
+
+			var binding = new Binding
+			{
+				Source = ViewModel,
+				Path = new PropertyPath($"[{i},{j}]"),
+				Mode = BindingMode.TwoWay
+			};
+
+			textBox.SetBinding(TextBox.TextProperty, binding);
+
+			return textBox;
+		}
+	}
+}
---
+++ SodukuModel/Grid.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuModel
+{
+	public class Grid
+	{
+		public static readonly int NB_CELLS = 9;
+		public static readonly int SQUARE_SIZE = 3;
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                               FIELDS                              *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		private Cell[,] cells;
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                             PROPERTIES                            *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                            CONSTRUCTORS                           *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		public Grid()
+		{
+			cells = new Cell[NB_CELLS, NB_CELLS];
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                           PUBLIC METHODS                          *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		public bool Solve(Cell[][] cells)
+		{
+			for (int x = 0; x < NB_CELLS; x++)
+			{
+				for (int y = 0; y < NB_CELLS; y++)
+				{
+					if (this[x, y] == null)
+					{
+						for (int n = 1; n <= NB_CELLS; n++)
+						{
+							if (IsSafe(x, y, n))
+							{
+								this[x, y] = n;
+								if (Solve(cells)) return true;
+								else this[x, y] = null;
+							}
+						}
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                          PRIVATE METHODS                          *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		/// <summary>
+		/// Checks if the number we are trying to place is already present in row, column or box
+		/// </summary>
+		/// <param name="row">The row</param>
+		/// <param name="col">The column</param>
+		/// <param name="number">The number we are trying to place</param>
+		/// <returns>If the grid is safe or not</returns>
+		private bool IsSafe(int row, int col, int number)
+		{
+			/* Check rows and columns, if the value is already there */
+
+			for (int k = 0; k < NB_CELLS; k++)
+			{
+				if (this[row, k] == number) return false;
+				if (this[k, col] == number) return false;
+			}
+
+			/* Check square boxes */
+
+			int gridRowStart = row - row % SQUARE_SIZE;
+			int gridColStart = col - col % SQUARE_SIZE;
+
+			for (int r = gridRowStart; r < gridRowStart + SQUARE_SIZE; r++)
+			{
+				for (int c = gridColStart; c < gridColStart + SQUARE_SIZE; c++)
+				{
+					if (this[r, c] == number) return false;
+				}
+			}
+
+			/* Otherwise, grid is safe */
+
+			return true;
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                         PROTECTED METHODS                         *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                          STATIC METHODS                           *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                         ABSTRACT METHODS                          *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                              INDEXERS                             *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		public int? this[int row, int col]
+		{
+			get { return cells[row, col].Value; }
+			set { cells[row, col].Value = value; }
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                         OPERATORS OVERLOAD                        *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	}
+}
